895a1192fb457d40e24098a6e328bc81886aa488
ksvvr <ksvvreddy@gmail.com>
2024-08-17 13:14:17 +0530

    tried to fetch products from api endpoint

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const http = require('http');
+const axios = require('axios');
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
-app.get("/", async (request, response) => {
-    response.render("homepage");
+app.get("/", (request, response) => {
+    response.render("homepage.ejs");
   });
-
 
 app.listen(3000, () => {
     console.log("Started express server at port 3000");
